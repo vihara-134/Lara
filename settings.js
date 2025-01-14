6e484d916558a936bb86a2259c82f6abaf7b0d0a
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2025-01-14 12:36:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vdQTTQAL#A5kMWXYQ1IJ2N32-wVpA_CpRzImtBkZEzPBBrXL2FFU", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "enwHTZ7S#8PlCl7OdPN-AA8fthAPo73B9bZv5ajyk6PK5NBDghzE", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUES_REPLY: process.env.STATUES_REPLY || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ANTI_LINK: process.env.ANTI_LINK || "true",
