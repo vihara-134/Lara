2acd9fb77d5e54cf9be13a241a606f688ba9ee8f
vihara-134 <viharavihara251@gmail.com>
2025-01-24 16:04:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,20 +5,20 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "DrZHWSAI#cZL5pRwZp4IWS9fKuDKhAWgt_LpATKJZBL3WbqkA09g", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-STATUES_REPLY: process.env.STATUES_REPLY || "true",
+STATUES_REPLY: process.env.STATUES_REPLY || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-ANTI_LINK: process.env.ANTI_LINK || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "flase",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 ANTI_BAD: process.env.ANTI_BAD || "true",   
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
-DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
-INBOX_BLOCK: process.env.INBOX_BLOCK || "false",
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '94761522163' : process.env.DELETEMSGSENDTO,
+INBOX_BLOCK: process.env.INBOX_BLOCK || "true",
 };
